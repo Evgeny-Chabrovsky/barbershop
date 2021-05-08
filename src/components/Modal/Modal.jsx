932855a20b6918303e8d925ac0d361eef8bf9f78
Evgeny-Chabrovsky <chabor111@gmail.com>
2021-05-08 13:41:07 +0300

    add routing with Promise in the Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import styles from "./Modal.module.css";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
 
 const Modal = ({ active, setActive }) => {
+  let history = useHistory();
+  function handleClick() {
+    history.push("/");
+  }
+  function handleRoute() {
+    const promise = new Promise((resolve, reject) => {
+      setActive(false);
+    });
+    promise.then(handleClick());
+  }
+
   return (
     <div
       className={active ? `${styles.modal} ${styles.active}` : styles.modal}
-      onClick={() => setActive(false)}
+      onClick={handleRoute}
     >
       <div
         className={styles.modal_content}
         onClick={(e) => e.stopPropagation()}
       >
-        {" "}
         <FontAwesomeIcon
-          onClick={() => setActive(false)}
+          onClick={handleRoute}
           icon={faTimes}
           className={styles.close_img}
         />
